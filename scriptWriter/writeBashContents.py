4c3f1b6f40ee358c3644a3554fbe2b9a619820e9
m-wkr <182266556+m-wkr@users.noreply.github.com>
2025-11-01 17:12:54 +0000

    Added helper function to create bash script

--- scriptWriter/writeBashContents.py
+++ scriptWriter/writeBashContents.py
@@ -1,6 +1,16 @@
+import os
+
 def writeToBashFile():
   return """
   #!/bin/bash
   
   python3 "../renderer/main.py
   """
+
+def createBashFile(filePath):
+  with open(filePath, "w") as f:
+    f.write(writeToBashFile())
+
+  os.chmod(filePath,0o755)
+
+createBashFile("123.sh")
