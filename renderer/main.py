677aef34525cd9c09a435d59033c90880f7ea373
Micfong <micfong2@outlook.com>
2025-11-02 11:30:28 +0000

    Accept additional arguments

--- renderer/main.py
+++ renderer/main.py
@@ -4,13 +4,11 @@ from playwright.sync_api import sync_playwright
 from domreader.domreader import dom_read
 from render import finder_render
 
-from converter.converter import CoordinateSystem
-
-
 def main():
     parser = ArgumentParser(description="Turns Finder to a web browser.")
-    parser.add_argument("--url", type=str, help="The URL to open. If plotting, this is ignored.")
-    parser.add_argument("--plot", action="store_true", help="Make a nice plot.")
+    parser.add_argument("-u", "--url", type=str, help="The URL to open. If plotting, this is ignored.")
+    parser.add_argument("-p", "--plot", action="store_true", help="Make a nice plot.")
+    parser.add_argument("-t", "--text", action="store_true", help="Use text-based rendering.")
     args = parser.parse_args()
 
     if not args.plot and not args.url:
@@ -23,11 +21,15 @@ def main():
         items = plotter_render()
         finder_render("Cam Hack 2025 - Plotter", items)
         return
+    
+    if args.text:
 
-    with sync_playwright() as playwright:
-        coords, title = dom_read(playwright, args.url)
-    finder_render(title, coords)
-
+        with sync_playwright() as playwright:
+            coords, title = dom_read(playwright, args.url)
+        finder_render(title, coords)
+        return
+    
+    # to be merged
 
 if __name__ == "__main__":
     main()
